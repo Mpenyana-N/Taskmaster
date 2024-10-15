666be89ef21c326f685c704cd342c2d00fc676c7
Mpenyana-N <nickmpenyana@gmail.com>
2024-10-15 04:37:19 +0200

    created entity

---
+++ src/main/java/entity/Task.java
@@ -0,0 +1,25 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.time.LocalDate;
+
+@Entity
+//Getter and setter from lombok.
+@Getter
+@Setter
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private boolean complete;
+    private LocalDate dueDate;
+}
